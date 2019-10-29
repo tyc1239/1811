f1ded93efc3e58236d1ce4959c03336f432f61e1
Your Name <you@example.com>
2019-10-29 19:45:52 +0800

    init

---
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Wechat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /*
+     * @content 微信链接开发者服务器
+     * */
+    
+}
